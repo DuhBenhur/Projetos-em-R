882fe42ca80e892c5fcb88ab268686f1e7e07f51
DuhBenhur <eduardo.benhur@gmail.com>
2022-09-04 23:24:07 -0300

    Data.frames, loops e condicionais - fim de capitulo.

--- Cap1.R
+++ Cap1.R
@@ -136,3 +136,96 @@ b_info$names[3]
 
 b_info$names[b_info$age_joined <= 14]
 
+
+#Data Frame
+names <- c("al","barbara","charles","donna","ellen","fred")
+height <- c(72,64,73,65,66,71)
+weight <- c(195,117,205,122,125,199)
+gender <- c("M","F","M","F","F","M")
+
+factor_gender <- factor(gender)
+factor_gender
+
+#A funcao data.frame() trabalha com vetroes para criar um dataframne
+d <- data.frame(names,factor_gender,height,weight)
+d
+
+#Altura da terceira pessoa
+
+d[3,3]
+
+# Todas informações da quinta pessoa
+
+d[5,]
+
+#Altura média
+
+mean(d$height)
+
+#critérios
+
+mean(d$heigth[d$factor_gender == "M"])
+
+
+#Outras formas
+#Acessar componente do dataframe sem $
+
+with(d,mean(height[factor_gender == "F"]))
+
+#É equivalente a
+mean(d$height[d$factor_gender == "F"])
+
+#Quantas linhas no dataframe
+
+nrow(d)
+
+#E quantas colunas
+
+ncol(d)
+
+#Adicionando colna com cbind()
+
+aptitude <- c(35,20,32,22,18,15)
+
+d.apt <- cbind(d,aptitude)
+d.apt
+
+# Loops for e instruções if
+
+#Formado geral do loop for
+
+for counter in star:end{
+  statement 1
+  statement n
+}
+#O counter controla as iterações
+
+#O formato geral mais simples de uma instrucao if é
+
+if(test){ declaracao para executar se o teste for verdadeiro}
+else {declaracao para executar se o teste for falso}
+
+xx <- c(2,3,4,5,6)
+xx
+yy <- NULL
+
+# Testar para ou impar mod no R é %%
+
+10%%3
+
+5%%2
+
+4%%2
+
+# if xx[i] %% 2 == 0 entãoo xx[i] é par
+
+#loop for e instrução if
+
+for(i in 1:length(xx)){
+  if(xx[i] %% 2 == 0){yy[i] <- "EVEN"}
+  else{ yy[i]<- "ODD"}}
+  
+
+
+yy
+###Final capitulo 1
