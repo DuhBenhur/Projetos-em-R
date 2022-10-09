1f6232030aac8d790155694c38e73c14c064514b
DuhBenhur <eduardo.benhur@gmail.com>
2022-10-09 02:05:13 -0300

    ANOVA de uma via

--- Fernanda_peres_estatisticaaplicada.R
+++ Fernanda_peres_estatisticaaplicada.R
@@ -1,4 +1,8 @@
 
+#Exercicios baseados na playlist Curso de Estatística Aplicada no R
+#Autor: Fernanda Peres
+#link: https://www.youtube.com/playlist?list=PLOw62cBQ5j9VE9X4cCCfFMjW_hhEAJUhU
+
 # Teste-t para duas amostras independentes ---------------------------------------------------
 
 
@@ -186,4 +190,165 @@ summary(dados2$Convulsoes_S1)
 
 describe(dados2$Convulsoes_PT)
 describe(dados2$Convulsoes_S1)
+
+
+
+# ANOVA de uma via --------------------------------------------------------
+
+# A ANOVA de uma via é um teste estatístico que permite a comparação das médias de mais de 2 grupos
+
+# O teste-t para amostras independentes, permite a comparação das médias para 2 grupos independentes.
+# A ANOVA de uma via é como se fosse uma extensão do teste-t para amostras independentes, permitindo a 
+# comparação de mais de 2 grupos independentes.
+
+#Passos do estudo. 
+
+# Verificar a normalidade por grupo (teste de Shapiro-Wilk);
+# Verificar a homogeneidade de variâncias (teste de Levene);
+# Verificar a presença de outliers (boxplot e função);
+# Fazer a ANOVA de 1 via;
+# Fazer diferentes tipos de post-hoc (Duncan, Bonferroni, Tukey HSD);
+# Pedir a estatística descritivaa dos dados
+
+pacotes <- c("dplyr","psych","car","RVAideMemoire","rstatix","DescTools")
+
+if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
+  instalador <- pacotes[!pacotes %in% installed.packages()]
+  for (i in 1:length(instalador)){
+    install.packages(instalador, dependencies = T)
+    break()
+  }
+  sapply(pacotes, require, character = T)
+}else{
+  sapply(pacotes, require, character = T)
+}
+
+
+dados <- read.csv2('Banco de Dados 5.csv')
+glimpse(dados)
+
+# Verificação da normalidade dos dados
+#Shapiro por grupo (pacote RVAdeMemoire)
+#Alternativas: Fazer um teste não paramétrico ou fazer a transformação dos dados.
+
+byf.shapiro(BC ~ Grupo, dados)
+#placebo não passou no teste de normalidade
+
+byf.shapiro(Pressao ~ Grupo, dados)
+
+#Verificação da homogeneidade de variancias
+
+leveneTest(BC ~ Grupo, dados, center=mean)
+leveneTest(Pressao ~ Grupo, dados, center=mean)
+
+#Observação
+#Por default, o teste realizado pelo pacote car tem como base a mediana(median)
+# O teste baseado na mediana é mais robusto
+# Mudamos para ser baseado na média (comparável ao SPSS)
+
+#Verificação da presença de outliers (por grupo) - Pacotes dplyr e rstatix
+
+#Para BC:
+dados %>% 
+  group_by(Grupo) %>% 
+  identify_outliers(BC)
+
+#pelo boxplot
+
+boxplot(BC ~ Grupo, data = dados, ylab = "Frequência cardíaca(bps)", xlab = "Tratamento")
+
+#Para Pressão:
+dados %>% 
+  group_by(Grupo) %>% 
+  identify_outliers(Pressao)
+
+#pelo boxplot
+
+boxplot(Pressao ~ Grupo, data = dados, ylab = "Frequência cardíaca(bps)", xlab = "Tratamento")
+
+#Realização da ANOVA
+
+#Criação do modelo para BC
+anova_BC <- aov(BC ~ Grupo, dados)
+summary(anova_BC)
+
+#H0: média do grupo A = média do grupo B = média do grupo C -> p > 0.05
+#H1: há pelo menos uma diferença entre as médias dos grupos -> p <= 0.05
+
+
+#Criação do modelo para Pressão
+anova_Pressao <- aov(Pressao ~ Grupo, dados)
+summary(anova_Pressao)
+
+
+#O resultado da ANOVA indicou que existem diferenças entre as médias dos grupos, mas 
+#não sabemos em qual grupo está a diferença.
+
+#Análise post-hoc 
+#Post-hocs permitidos: "hsd", "bonferroni", "lsd", "scheffe", "newmankeuls", "duncan"
+
+#Observação
+
+#Existe uma quantidade muito grande de testes de post-hocs no mundo
+# O adequado vai depender da necessidade de pesquisa
+# Em geral os testes de post-hoc podem ser divididos em testes mais liberais e mais conservadores
+
+#Os testes mais liberais, corrigem menos, então é mais provavel que se encontre diferenças
+# em testes mais liberais e menos provavel que se encontre diferenças em testes mais conservadores. 
+
+#Os testes mais liberais cometem mais erros do tipo 1: Dizer que uma coisa é significante quando na verdade
+#ela não é
+
+#Isso depende muito do seu desenho experimental, dependendo da sua pergunta, vocè pode cometer erro do tipo
+# 1 mas não pode cometer erro do tipo 2 e vice-versa
+
+# O ideal é usar um post-hoc equilibrado, nem tão liberal e nem tão conservador
+
+# vamos fazer 3
+
+# Um liberal: duncan
+# Um moderado: TukeyHSD (HSD = Diferença Significante Honesta )
+# Um Conservador: Bonferroni
+
+#Uso do Duncan
+PostHocTest(anova_BC, method = "duncan", conf.level = 0.95)
+
+
+#Uso do TukeyHSD
+PostHocTest(anova_BC, method = "hsd", conf.level = 0.95)
+
+
+#Uso do Bonferroni
+PostHocTest(anova_BC, method = "bonf", conf.level = 0.95)
+
+#É o mesmo que:
+
+pairwise.t.test(dados$BC, dados$Grupo, p.adj = "bonferroni", paired = F)
+
+#Exemplo de como resumir em uma tabela mais de um post-hoc
+
+round(
+  cbind(duncan = PostHocTest(anova_BC, method = "duncan")$Grupo[,"pval"],
+        bonf = PostHocTest(anova_BC, method = "bonf")$Grupo[,"pval"],
+        hsd = PostHocTest(anova_BC, method = "hsd")$Grupo[,"pval"]),6)
+
+
+#para pressão
+
+#Uso do TukeyHSD
+PostHocTest(anova_Pressao, method = "hsd", conf.level = 0.95)
+
+
+#Analise descritiva dos dados
+
+describeBy(dados$BC, group = dados$Grupo)
+describeBy(dados$Pressao, group = dados$Grupo)
+
+#Descrevendo resultados da ANOVA para pressão
+
+#A ANOVA de uma via mostrou que há efeito do tratamento sobre a média de pressão 
+#arterial [F(2,28) = 5,521; P = 0,0095]. O post-hoc Tukey HSD mostrou que há
+#diferenças entre o grupo placebo e o grupo anti-hipertensivo padrão, mas nao entre os demais
+#grupos
+summary(anova_Pressao)
 
