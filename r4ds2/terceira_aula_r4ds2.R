3076bf768fe67421783ba8a49a91214db2989d98
DuhBenhur <eduardo.benhur@gmail.com>
2022-11-06 23:01:14 -0300

    strings e inicio das regex

--- r4ds2/terceira_aula_r4ds2.R
+++ r4ds2/terceira_aula_r4ds2.R
@@ -11,3 +11,81 @@ c(1,2,3,NA,4) |>
   purrr::discard(is.na) #Talvez esse não seja o método que o R prefere para vetores
 
 
+#Motivação
+
+#Bases com colunas em texto já são extremamente comuns hoje em dia, então saber lidar 
+#com etrings se torna essencial na caixa de ferramentas do cientista de daods
+
+#Além de ajudar em análise de dados, tratar strings ajuda com programação
+#porque grande parte das linguagens modernas funcionamda mesma maneira que o R
+#nesse quesito.
+
+#O conhecimento de expressões regulares vale para a vida, é impossível descrever
+#com poucas palavras todas as coisas que são implementáveis via regex
+
+#Normalmente os textos são bagunçados, independentemente do quão cuidadosa foi
+#a coleta de dados, então precisamos arrumá-los; podemos fazer isso do jeito fácil
+#({stringr} e regex) ou do jeito difícil ({base}e lágrimas ) hauhauhaua
+
+
+#Strings não passam de sequências de caracteres ("cadeias" em português)
+
+#No R podemos criar uma string com um par de aspas(simples ou duplas)
+
+#O print() mostra a estrutura da string, enquanto cat()mostra o texto
+
+print("etc! Está 10\u00BAC lá fora")
+
+#Para colocar aspas dentro de uma string, podemos escapas o caractere
+
+cat("Ele disse \"escapar\"")
+
+
+#O pacote {stringr} é a forma mais simples de trabalhar com strings no R
+
+library(stringr)
+
+abc <- c("a", "b", "c")
+str_c("prefixo-", abc,"-sufixo")
+
+
+#Todas as funções relevantes começam com str_e funcionam bem juntas
+
+abc |>
+  str_c("-sufixo")|>
+  str_length()
+
+
+#Encontrando padrões
+str_detect("Colando Strings", pattern = "ando")
+
+#Extraindo trecho padrão
+
+str_extract("Colando Strings", pattern = "ando")
+
+#Substituindo padrões
+
+str_replace("Colando Strings", pattern = "ando", replacement = "ei")
+
+
+#Removendo padrões
+
+str_remove("Colando Strings", pattern = "Strings") %>% 
+  str_squish() #A função trimws(), não ajusta espaços no meio, apenas no começo e no final
+
+
+#Regex
+
+#Expressões regulares "são "programação pa strings", permitindo extrair padrões
+#bastante complexos com comandos simples
+
+#Elas giram em torno de padrões "normais" de texto, mas com alguns símbolos especiais
+#com significados específicos
+
+frutas <- c("banana", "TANGERINA", "maçã", "lima")
+
+str_detect(frutas, pattern = "na")
+
+#Exemplo qualquer caractere ^ (início da string) e $(fim da string)
+
+str_detect(frutas, pattern = "^ma")
